8203c3d190d0068810198e4de67ab2de27ed9789
Andres Reyes <areyesdev@gmail.com>
2021-12-20 22:29:48 -0500

    feat: MediumCard and ScrollBar Hide

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
 export default function Home({ exploreData, cardsData }) {
@@ -28,6 +29,11 @@ export default function Home({ exploreData, cardsData }) {
         </section>
         <section>
           <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
